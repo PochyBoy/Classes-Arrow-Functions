c785e131906f3eb47ce8c74cc8f7837e6dc6514a
Paul Vargas Portugal <videoblogpaul@gmail.com>
2023-01-26 10:11:17 +0100

    Nivel 2 completado

--- Nivel2.js
+++ Nivel2.js
@@ -9,3 +9,23 @@ let nombre = (nombre) => {
 nombre('Paul Vargas Portugal')
 
 /** Ejercicio 2 **/
+
+class Persona {
+    constructor(nombre){
+        this.nombre = nombre
+        
+    }
+    get Nombre(){
+        return this.muestraNombre()
+    }
+    
+    muestraNombre(){
+        return console.log(this.nombre)
+    }
+
+}
+
+const Paul = new Persona ('Paul')
+
+Paul.Nombre
+
