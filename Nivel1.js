1ecf9d07f725e77af95b29a26f1a80a735b07aa3
Paul Vargas Portugal <videoblogpaul@gmail.com>
2023-02-01 10:21:26 +0100

    uniendo datos

--- Nivel1.js
+++ Nivel1.js
@@ -1,4 +1,7 @@
 
+
+//Nivel 1
+
 /** Ejercicio 1 **/
 
 let suma = (numero1,numero2) => {
@@ -8,3 +11,51 @@ let suma = (numero1,numero2) => {
 suma (5, 20)
 
 
+//Nivel 2
+/** Ejercicio 1 **/
+
+let nombre = (nombre) => {
+    let objeto = {nombre: nombre}
+
+    console.log(objeto)
+}
+
+nombre('Paul Vargas Portugal')
+
+/** Ejercicio 2 **/
+
+class Persona {
+    constructor(nombre){
+        this.nombre = nombre
+        
+    }
+    get Nombre(){
+        return this.muestraNombre()
+    }
+    
+    muestraNombre(){
+        return console.log(this.nombre)
+    }
+
+}
+
+const Paul = new Persona ('Paul')
+
+Paul.Nombre
+
+
+
+//Nivel 3
+class Cursos {
+    constructor(back, front) {
+      this.back = back;
+      this.front = front;
+    }
+  }
+  
+  function creadorObjetos( backend, frontend) {
+     return new Cursos( backend, frontend );
+  }
+  console.log(creadorObjetos('NodeJs', 'React'))
+  
+  console.log(creadorObjetos('Java', 'Angular'))
